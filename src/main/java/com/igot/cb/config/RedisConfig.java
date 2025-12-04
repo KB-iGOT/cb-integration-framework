9eda01279e4e1aa86198ecdbe72b4196b94670e9
arpithasureshappa <arpitha.sureshappa@tarento.com>
2025-12-04 12:22:03 +0530

    redis Ip updated to 8.x

--- src/main/java/com/igot/cb/config/RedisConfig.java
+++ src/main/java/com/igot/cb/config/RedisConfig.java
@@ -1,9 +1,13 @@
 package com.igot.cb.config;
 
 import com.igot.cb.model.ResponseDTO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -11,6 +15,22 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.redis.database}")
+    private int redisDatabase;
+
+    @Bean
+    @Primary
+    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory(
+            @Value("${spring.redis.host}") String host,
+            @Value("${spring.redis.port}") int port) {
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        config.setDatabase(redisDatabase);
+        return new LettuceConnectionFactory(config);
+    }
 
     @Bean
     public ReactiveRedisTemplate<String, ResponseDTO> redisOperations(ReactiveRedisConnectionFactory factory) {
@@ -23,5 +43,4 @@ public class RedisConfig {
 
         return new ReactiveRedisTemplate<>(factory, context);
     }
-
 }
